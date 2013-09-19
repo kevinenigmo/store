84abd2a4dc18b662d79ff2b9d36aa649a1b5f0f5
kevin <kevin@enigmo.co.jp>
2013-09-19 14:09:06 +0900

    grid

--- app/View/Products/index.ctp
+++ app/View/Products/index.ctp
@@ -16,36 +16,42 @@
 <table class="table table-striped">
 	
 	<tr>
-		<th>ID</th>
-		<th>Item image</th>
-		<th>Item name</th>
-		<th>Item comment</th>
-		<th>Item left</th>
+		<th>sort</th>
 		<th><?php echo $this->Paginator->sort('price', 'Price') ?></th>
-		<th>Release date</th>
 		<th><?php echo $this->Paginator->sort('Brand.brand_name', 'Brand') ?></th>
-		<th>Buyer name</th>
 	</tr>
 	
-	<?php foreach ($products as $product): ?>
-	<tr>
-		<td><?php echo $product['Product']['id'] ?></td>
-		<td><?php if ($product['Product']['image']) { echo $this->Html->image('products/' . $product['Product']['id'] .'/small_'. $product['Product']['image']);} ?>
-		<td><?php echo $this->Html->link($product['Product']['item_title'], array('controller' => 'products', 'action' => 'detail', $product['Product']['id'])) ?></td>
-		<td><?php echo $this->Text->truncate($product['Product']['item_comment'], 100, array('ellipsis' => '..')) ?></td>
-		<td><?php echo $product['Product']['items_left'] ?></td>
-		<td><?php echo $this->Number->currency($product['Product']['price'], 'JPY') ?></td>
-		<td><?php echo $this->Time->format($product['Product']['release_date'], '%Y/%m/%d') ?></td>
-		<td><?php echo $product['Brand']['brand_name'] ?><br /><?php echo $product['Brand']['brand_name_kana'] ?></td>
-		<td><?php echo $product['User']['username'] ?></td>
-		<?php if($userData['id'] === $product['User']['id'] || $userData['admin'] ==='1'): ?>
-		<td><?php echo $this->Html->link('Edit', array('action' => 'edit', $product['Product']['id'])) ?>
-		<?php endif ?>
-	</tr>
-	<?php endforeach ?>
-	<?php unset($product) ?>
-
 </table>
+
+<?php $c = 0;
+	foreach ($products as $product):
+	if ($c % 4 == 0) { 
+?>
+	<div class="row" id="equalize" style="padding: 10px">
+	<?php }; ?>
+	
+	<div class="col-sm-6 col-md-3">
+	    <div class="thumbnail">
+	      <?php if ($product['Product']['image']) { echo $this->Html->image('products/' . $product['Product']['id'] .'/small_'. $product['Product']['image']);} ?>
+	      <div class="caption">
+	        <h3><?php echo $this->Html->link($product['Product']['item_title'], array('controller' => 'products', 'action' => 'detail', $product['Product']['id'])) ?></h3>
+	        <p><?php echo $this->Text->truncate($product['Product']['item_comment'], 100, array('ellipsis' => '..')) ?></p>
+	        <p><?php echo $this->Number->currency($product['Product']['price'], 'JPY') ?></p>
+	        <p><?php echo $product['User']['username'] ?></p>
+	        <?php if($userData['id'] === $product['User']['id'] || $userData['admin'] ==='1'): ?>
+			<p><?php echo $this->Html->link('Edit', array('action' => 'edit', $product['Product']['id'])) ?></p>
+			<?php endif ?>
+	      </div>
+	    </div>
+  	</div>
+	<?php if ($c % 4 == 3) { ?>
+	</div>
+	<?php }; 
+		$c++;
+	?>
+
+<?php endforeach ?>
+<?php unset($product) ?>
 
 <div class="pagination">
 	<?php if ($this->Paginator->hasPrev()) {echo $this->Paginator->prev('<<Prev', null, null, array('class' => 'disabled'));} ?>
---
+++ shop.sql
@@ -0,0 +1,102 @@
+-- MySQL dump 10.13  Distrib 5.1.69, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: webstore
+-- ------------------------------------------------------
+-- Server version	5.1.69-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `brands`
+--
+
+DROP TABLE IF EXISTS `brands`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `brands` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `brand_name` varchar(100) DEFAULT NULL,
+  `brand_name_kana` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `product_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_title` varchar(255) DEFAULT NULL,
+  `item_comment` text,
+  `items_left` int(11) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `release_date` datetime DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `brand_id` int(11) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10275949 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT 'default@enigmo.com',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `password` varchar(50) DEFAULT 'defaultPassword',
+  `admin` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1646136 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-09-19  4:30:24
